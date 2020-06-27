af7a8dcf07e40d4289491d5fd628a4b24fba84e8
Stephan-MDD <stephan.mdd@gmail.com>
2020-06-27 14:08:11 +0200

    added react router

--- client/src/routes/Root.js
+++ client/src/routes/Root.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import '../sass/Root.scss';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 const Root = () => {
 	return (
-		<div className="App">
-			<h2>hello world</h2>
-		</div>
+		<Router className="App">
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="*">
+					<Error />
+				</Route>
+			</Switch>
+		</Router>
 	);
+};
+
+const Home = () => {
+	return <h2>Home</h2>;
+};
+
+const Login = () => {
+	return <h2>Login</h2>;
+};
+
+const Error = () => {
+	return <h2>Error</h2>;
 };
 
 export default Root;
